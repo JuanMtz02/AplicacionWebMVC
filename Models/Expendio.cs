4b93e400c027357fc345fd46bad70580faec8d67
PC01-1H <PC01-1H@PC23-1H>
2024-02-19 12:49:51 -0600

    Agregar archivos de proyecto.

---
+++ Models/Expendio.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AplicacionWebMVC.Models
+{
+    public class Expendio
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdC { get; set; }
+        public string Nombre { get; set; }
+        public string TipoC { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
